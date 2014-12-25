bc8d02514198ede72da79dc3e012850edc9db871
ZJUZT <961160308@qq.com>
2014-12-25 18:26:58 +0800

    add Frozen

---
+++ Assets/Scripts/Frozen.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frozen : MonoBehaviour {
+
+    protected ArrayList mylist1;
+    protected ArrayList mylist2;
+    public float m_livetime = 5;
+	// Use this for initialization
+	void Start () {
+
+        mylist1 = new ArrayList();
+        mylist2 = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_livetime -= Time.deltaTime;
+        if (m_livetime <= 0)
+        {
+            
+
+            //free the ball being frozen
+            foreach(RedDot1 reddot in mylist1)
+            {
+                reddot.isFrozen = false;
+            }
+            foreach (RedDot2 reddot in mylist2)
+            {
+                reddot.isFrozen = false;
+            }
+
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Reddot_1"))
+        {
+            RedDot1 red1 = other.GetComponent<RedDot1>();
+            if(red1!=null)
+            {
+                red1.isFrozen = true;          //froze the ball
+                mylist1.Add(red1);
+            }
+            
+        }
+        else if(other.tag.Equals("Reddot_2"))
+        {
+            RedDot2 red2 = other.GetComponent<RedDot2>();
+            if(red2!=null)
+            {
+                red2.isFrozen = true;
+                mylist2.Add(red2);
+            }
+           
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/iceItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iceItem : MonoBehaviour {
+
+    public Transform m_Frozen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            Destroy(this.gameObject);
+            Instantiate(m_Frozen, this.transform.position, Quaternion.identity);
+        }
+    }
+}
