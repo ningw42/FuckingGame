d6624429a83ce05f6ed6e745cb09e58fcbf8e6a1
unidentifiedme <unidentifiedme26@gmail.com>
2014-12-25 17:47:36 +0800

    name changes

---
+++ Assets/Scripts/blackHole.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class blackHole : MonoBehaviour {
+	public Vector3 gravity;
+    public Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter( Collider other )
+	{ 
+	} 
+
+	void OnTriggerStay( Collider other)
+	{
+        GameObject inrangeObject = other.gameObject;
+        if ((inrangeObject.transform.position - m_transform.position).magnitude < 5)
+        {
+            Debug.Log(inrangeObject.tag);
+            if (!other.tag.Equals("Player"))
+            {
+                GameManager.Instance.AddScore(1);
+                Destroy(inrangeObject);
+            }
+        }
+        else
+        {
+            gravity = m_transform.position - inrangeObject.transform.position;
+            gravity = gravity.normalized * 100 / (gravity.magnitude < 5 ? 5 : gravity.magnitude);
+            inrangeObject.transform.Translate(gravity, Space.World);
+        }
+	}
+
+	void OnTriggerExit( Collider other )
+	{
+	} 
+
+	void FixedUpdate ()
+	{
+	}
+}
